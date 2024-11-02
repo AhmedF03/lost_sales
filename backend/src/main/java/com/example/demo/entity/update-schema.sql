dce9fe8f1bd6a9233809c173805edc8e03968fc2
AhmedF03 <[ahmedabdelbary44@gmail.com]>
2024-11-01 22:32:15 -0400

    implemented backend, added frontend

---
+++ backend/src/main/java/com/example/demo/entity/update-schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE item
+(
+    id            BIGINT AUTO_INCREMENT NOT NULL,
+    `description` VARCHAR(255)          NULL,
+    completed     BIT(1)                NOT NULL,
+    CONSTRAINT pk_item PRIMARY KEY (id)
+);
